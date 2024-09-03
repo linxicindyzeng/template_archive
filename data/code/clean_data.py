7d44ab8926a576e46ef41e40dc3a90e97e3e92af
Cindy <135077841+linxicindyzeng@users.noreply.github.com>
2024-09-03 11:21:09 -0700

    PR for #1: Modify chips sold histogram to reflect percentages instead of counts (#2)
    * #1 Modify chips sold histogram to reflect percentages instead of counts
    
    * #3 Remove 6 files committed by mistake

--- data/code/clean_data.py
+++ data/code/clean_data.py
@@ -10,11 +10,17 @@ def main():
     df.to_csv('output/data_cleaned.csv', index = False)
 
 def plot_data(df):
-    plt.hist(df['chips_sold'])
+
+    plt.hist(df['chips_sold'], weights = 100 * np.ones(len(df['chips_sold'])) / len(df['chips_sold']))
+
+    plt.xlabel('Chips Sold')
+    plt.ylabel('Percentage')
+    plt.title('Percentages of Chips Sold')
+
     plt.savefig('output/chips_sold.pdf')
 
 def clean_data(df):
-    df['chips_sold'][df['chips_sold'] == -999999] = np.NaN
+    df['chips_sold'][df['chips_sold'] == -999999] = np.nan
     return(df)
     
 ### EXECUTE
